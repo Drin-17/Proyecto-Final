e52ce55e7eb7bbe746c71c60a59143ea178252ed
Rodrigo Naranjo <rodrinacan15@gmail.com>
2023-11-07 16:32:18 -0500

    Agregar archivos de proyecto.

---
+++ Poryecto Final/Models/Spotify/Spotify.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace Proyecto_Final.Models.Spotify
+{
+    public class Spotify
+    {
+       
+
+        public char url { get; set; }
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Spotify(char url, long id, string name)
+        {
+            this.url = url;
+            Id = id;
+            Name = name;
+        }
+    }
+    
+
+
+}
